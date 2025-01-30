2ed5200bfc9961f96d2dd44064bdfa965cafee8e
Amulya Jain <amulyajain123@gmail.com>
2025-01-31 00:55:42 +0530

    bug fix

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = async () => {
     try {
-        const str = process.env.STATUS === 'dev' ? process.env.CONNECTION_STR_DEV : process.env.CONNECTION_STR_PROD;;
+        const str = process.env.STATUS === 'dev' ? process.env.CONNECTION_STR_DEV : process.env.CONNECTION_STR_PROD;
         await mongoose.connect(str);
     } catch (error) {
         throw error;
